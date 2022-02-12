ae1ac48c25c24beac7c7b2dd6b546d76fccc9573
Wish-nu <81408708+Wish-nu@users.noreply.github.com>
2022-02-12 12:30:58 +0530

    Create MyPet.java

---
+++ MyPet.java
@@ -0,0 +1,7 @@
+public class MyPet {
+    public static void main(String args[]) {
+      MyPet dog = new MyPet();
+      System.out.println(dog);
+      // printing the class MyPet with some memory allocation
+    }
+}
