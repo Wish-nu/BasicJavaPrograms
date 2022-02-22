c863dfcfcbd013dd0ed8226f4e4f0043f2a110c1
Wish-nu <81408708+Wish-nu@users.noreply.github.com>
2022-02-23 00:30:08 +0530

    default constructor example

---
+++ DefaultConstructor.java
@@ -0,0 +1,10 @@
+class Customer {
+
+    Customer(){} // this is an example of default constructor
+    
+    public byte age=45;
+    public String firstName ="Edward";
+    public String lastName ="Snowden";
+    private long bankBalance= 500000000L;
+    private String password = "Bringmetolife";
+    }
