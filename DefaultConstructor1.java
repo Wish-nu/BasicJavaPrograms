89cfdfd9bf700002b296d3059905df3db4056896
Wish-nu <81408708+Wish-nu@users.noreply.github.com>
2022-02-26 10:48:31 +0530

    invoking a default constructor

---
+++ DefaultConstructor1.java
@@ -0,0 +1,12 @@
+
+public class Car1 {
+    Car1(){     //created a default constructor
+        System.out.println("Car is created");
+    }
+    //main method
+    public static void main(String[] args) {
+        // calling out the default constructor
+        Car1 Bugatti = new Car1();
+        System.out.println(Bugatti); 
+    }
+}
